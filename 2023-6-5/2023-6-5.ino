744c9d42c80db912168217d7156067757bfb7bfb
bobbycumt <bobbycumt@163.com>
2023-08-05 07:08:42 +0800

    修改了一部分代码

--- 2023-6-5/2023-6-5.ino
+++ 2023-6-5/2023-6-5.ino
@@ -1,10 +1,11 @@
 #include <IRremote.h>
-
+unsigned long code;
 void setup()
 {
   // put your setup code here, to run once:
   IrReceiver.begin(2);
   Serial.begin(9600);
+  Serial.println("123");
 }
 
 void loop()
@@ -12,8 +13,13 @@ void loop()
   // put your main code here, to run repeatedly:
   if (IrReceiver.decode())
   {
-    unsigned long code = IrReceiver.decodedIRData.decodedRawData;
+    code = IrReceiver.decodedIRData.decodedRawData;
+    code>>=16;
     Serial.println(code,HEX);
     IrReceiver.resume();
+  }  
+  if(code==0xf310)
+  {
+    Serial.println("1");
   }
 }
