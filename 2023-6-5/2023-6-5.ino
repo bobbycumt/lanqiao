10fc6e0deec9902efe52347d835040677b45c21c
bobbycumt <bobbycumt@163.com>
2023-08-05 16:26:28 +0800

    更新程序和题解

--- 2023-6-5/2023-6-5.ino
+++ 2023-6-5/2023-6-5.ino
@@ -1,25 +1,112 @@
-#include <IRremote.h>
-unsigned long code;
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(32, 16, 2);
+int n, c = 0, i;
+char s[20];
 void setup()
 {
   // put your setup code here, to run once:
-  IrReceiver.begin(2);
-  Serial.begin(9600);
-  Serial.println("123");
+  for (int i = 5; i <= 7; i++)
+    pinMode(i, OUTPUT);
+  for (int i = 5; i <= 7; i++)
+    digitalWrite(i, 1);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(39, INPUT);
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (IrReceiver.decode())
+  delay(3000);
+  lcd.clear();
+  lcd.print("ready go!");
+  for (int i = 5; i <= 7; i++)
+    analogWrite(i, 255);
+  n = random(5, 8);
+  switch (n)
   {
-    code = IrReceiver.decodedIRData.decodedRawData;
-    code>>=16;
-    Serial.println(code,HEX);
-    IrReceiver.resume();
-  }  
-  if(code==0xf310)
-  {
-    Serial.println("1");
+  case 5:
+    /* code */
+    i = 0;
+    digitalWrite(6, 1);
+    digitalWrite(5, 0);
+    digitalWrite(7, 1);
+    delay(1000);
+    while (i <= 10)
+    {
+      if (digitalRead(39))
+      {
+        c++;
+        lcd.clear();
+        sprintf(s, "good job!x %d", c);
+        lcd.print(s);
+        break;
+      }
+      else if (analogRead(A6) <= 500 or (analogRead(A6) >= 600 and analogRead(A6) <= 1023))
+      {
+        lcd.clear();
+        lcd.print("come on!!!");
+      }
+      i++;
+      delay(300);
+    }
+    break;
+  case 6:
+    /* code */
+    i = 0;
+    digitalWrite(6, 0);
+    digitalWrite(5, 1);
+    digitalWrite(7, 1);
+    delay(1000);
+    while (i <= 10)
+    {
+      if (analogRead(A6) >= 600 and analogRead(A6) <= 1023)
+      {
+        c++;
+        lcd.clear();
+        sprintf(s, "good job!x %d", c);
+        lcd.print(s);
+        break;
+      }
+      else if (analogRead(A6) <= 500 or digitalRead(39))
+      {
+        lcd.clear();
+        lcd.print("come on!!!");
+      }
+      i++;
+      delay(300);
+    }
+    break;
+  case 7:
+    /* code */
+    i = 0;
+    digitalWrite(6, 0);
+    digitalWrite(5, 0);
+    digitalWrite(7, 1);
+    delay(1000);
+    while (i <= 10)
+    {
+      if (analogRead(A6) <= 500)
+      {
+        c++;
+        lcd.clear();
+        sprintf(s, "good job!x %d", c);
+        lcd.print(s);
+        break;
+      }
+      else if ((analogRead(A6) >= 600 and analogRead(A6) <= 1023) or digitalRead(39))
+      {
+        lcd.clear();
+        lcd.print("come on!!!");
+      }
+      i++;
+      delay(300);
+    }
+    break;
+  default:
+    break;
   }
 }
