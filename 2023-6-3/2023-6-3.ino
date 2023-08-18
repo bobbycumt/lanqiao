62919f1bff85a15ea65e582aae8c3647c9d1ca68
bobbycumt <bobbycumt@163.com>
2023-08-18 10:47:17 +0800

    Update 2023-6-3.ino

--- 2023-6-3/2023-6-3.ino
+++ 2023-6-3/2023-6-3.ino
@@ -8,7 +8,7 @@ void setup()
         pinMode(i, OUTPUT);
     for (int i = 31; i <= 38; i++)
         digitalWrite(i, 1);
-    attachInterrupt(4, f, RISING);
+    attachInterrupt(digitalPinToInterrupt(19), f, RISING);
 }
 void loop()
 {
