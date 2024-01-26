4ae1cf5e5682a9f1965ec1380995e0d222a264a0
bobbycumt <bobbycumt@163.com>
2024-01-26 12:00:37 +0800

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,58 +1,20 @@
-String s = "";
-int a, b = 0;
+int i;
+
 void setup()
 {
+    i = 15;
     Serial.begin(9600);
+    for (i = 1; i <= 10; i = i + (1))
+    {
+        Serial.println(i);
+    }
+    for (i = 10; i >= 1; i = i + (-1))
+    {
+        Serial.println(i);
+    }
+    Serial.println(i);
 }
 
 void loop()
 {
-    Serial.println("****请选择功能****");
-    Serial.println("1->圆面积计算");
-    Serial.println("2->长方形面积计算");
-    Serial.println("3->灯控制");
-    while (!Serial.available())
-        ;
-    while (Serial.available())
-    {
-        s = Serial.readString();
-        s.trim();
-    }
-    if (s.toInt() == 1)
-    {
-        Serial.println("请输入半径值：");
-        while (!Serial.available())
-            ;
-        while (Serial.available())
-        {
-            s = Serial.readString();
-            s.trim();
-        }
-        Serial.println(s.toInt() * 2);
-        s = "";
-    }
-    else if (s.toInt() == 2)
-    {
-        Serial.println("请输入a：");
-        while (!Serial.available())
-            ;
-        while (Serial.available())
-        {
-            s = Serial.readString();
-            s.trim();
-        }
-        a = s.toInt();
-
-        Serial.println("请输入b：");
-        while (!Serial.available())
-            ;
-        while (Serial.available())
-        {
-            s = Serial.readString();
-            s.trim();
-        }
-        b = s.toInt();
-        Serial.println(a * b);
-        s = "";
-    }
 }
